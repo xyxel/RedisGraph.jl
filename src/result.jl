0fba73753d3026d932d8e712ea0bad8be94d1e8e
ikudrautsau <ihar.kudrautsau@gmail.com>
2019-09-06 11:24:19 +0300

    use multiple dispatch for result parsing

--- src/result.jl
+++ src/result.jl
@@ -5,44 +5,42 @@ const SCALAR_TYPE_INTEGER = 3
 const SCALAR_TYPE_BOOLEAN = 4
 const SCALAR_TYPE_DOUBLE = 5
 
+struct SCALAR_TYPE{x}
+end
+
+SCALAR_TYPE(x) = SCALAR_TYPE{x}()
+
 const ENTRY_TYPE_UNKNOWN = 0
 const ENTRY_TYPE_SCALAR = 1
 const ENTRY_TYPE_NODE = 2
 const ENTRY_TYPE_RELATION = 3
 
-
-function parsescalar(raw_value_tuple)
-    scalar_type = raw_value_tuple[1]
-    raw_value = raw_value_tuple[2]
-    if scalar_type == SCALAR_TYPE_NULL
-        value = nothing
-    elseif scalar_type == SCALAR_TYPE_STRING
-        value = raw_value  # string(raw_value)
-    elseif scalar_type == SCALAR_TYPE_INTEGER
-        value = raw_value  # parse(Int, raw_value)
-    elseif scalar_type == SCALAR_TYPE_BOOLEAN
-        value = parse(Bool, raw_value)
-    elseif scalar_type == SCALAR_TYPE_DOUBLE
-        value = parse(Float64, raw_value)
-    else
-        @assert false "Unknown scalar type"
-    end
-    value
+struct ENTRY_TYPE{x}
 end
 
+ENTRY_TYPE(x) = ENTRY_TYPE{x}()
 
-function parseprops(g::Graph, raw_props)
+
+function parsescalar(::SCALAR_TYPE{SCALAR_TYPE_UNKNOWN}, raw_value::String) @assert false "Unknown scalar type" end
+function parsescalar(::SCALAR_TYPE{SCALAR_TYPE_NULL}, raw_value::String) return nothing end
+function parsescalar(::SCALAR_TYPE{SCALAR_TYPE_STRING}, raw_value::String) return raw_value end
+function parsescalar(::SCALAR_TYPE{SCALAR_TYPE_INTEGER}, raw_value::Int) return raw_value end
+function parsescalar(::SCALAR_TYPE{SCALAR_TYPE_BOOLEAN}, raw_value::String) return parse(Bool, raw_value) end
+function parsescalar(::SCALAR_TYPE{SCALAR_TYPE_DOUBLE}, raw_value::String) return parse(Float64, raw_value) end
+
+
+function parseprops(g::Graph, raw_props::Array{Any})
     props = Dict()
     for raw_prop in raw_props
         prop_name = getprop(g, raw_prop[1])
-        prop_value = parsescalar(raw_prop[2:length(raw_prop)])
+        prop_value = parsescalar(SCALAR_TYPE(raw_prop[2]), raw_prop[3])
         props[prop_name] = prop_value
     end
     return props
 end
 
 
-function parsenode(g::Graph, raw_entry)
+function parsenode(g::Graph, raw_entry::Array{Any})
     node_id = raw_entry[1]
     label = NaN
     if length(raw_entry[2]) != 0
@@ -53,7 +51,7 @@ function parsenode(g::Graph, raw_entry)
 end
 
 
-function parseedge(g::Graph, raw_entry)
+function parseedge(g::Graph, raw_entry::Array{Any})
     edge_id = raw_entry[1]
     relation = getrelation(g, raw_entry[2])
     src_node_id = raw_entry[3]
@@ -63,22 +61,13 @@ function parseedge(g::Graph, raw_entry)
 end
 
 
-function parseentry(g::Graph, raw_entry, entry_type::UInt)
-    if entry_type == ENTRY_TYPE_UNKNOWN
-        @assert false "Unknown result entry"
-    elseif entry_type == ENTRY_TYPE_SCALAR
-        return parsescalar(raw_entry)
-    elseif entry_type == ENTRY_TYPE_NODE
-        return parsenode(g, raw_entry)
-    elseif entry_type == ENTRY_TYPE_RELATION
-        return parseedge(g, raw_entry)
-    else
-        @assert false "Unknown entry type"
-    end
-end
+function parseentry(g::Graph, raw_entry::Array{Any}, ::ENTRY_TYPE{ENTRY_TYPE_UNKNOWN}) @assert false "Unknown result entry" end
+function parseentry(g::Graph, raw_entry::Array{Any}, ::ENTRY_TYPE{ENTRY_TYPE_SCALAR}) return parsescalar(SCALAR_TYPE(raw_entry[1]), raw_entry[2]) end
+function parseentry(g::Graph, raw_entry::Array{Any}, ::ENTRY_TYPE{ENTRY_TYPE_NODE}) return parsenode(g, raw_entry) end
+function parseentry(g::Graph, raw_entry::Array{Any}, ::ENTRY_TYPE{ENTRY_TYPE_RELATION}) return parseedge(g, raw_entry) end
 
 
-function parsestatistics(raw_stats::Vector{Any})
+function parsestatistics(raw_stats::Array{Any})
     statistics = Dict()
     for stat in raw_stats
         stat_name, stat_value = split(stat, ": ")
@@ -88,7 +77,7 @@ function parsestatistics(raw_stats::Vector{Any})
 end
 
 
-function parseresults(g::Graph, raw_results)
+function parseresults(g::Graph, raw_results::Array{Any})
     header = nothing
     results = nothing
     if length(raw_results) != 0
@@ -96,7 +85,7 @@ function parseresults(g::Graph, raw_results)
         results = Vector{Any}()
         for row in raw_results[2]
             for (header_entry, raw_result_entry) in zip(header, row)
-                entry = parseentry(g, raw_result_entry, header_entry.r_entry_type)
+                entry = parseentry(g, raw_result_entry, ENTRY_TYPE(header_entry.r_entry_type))
                 push!(results, entry)
             end
         end
@@ -105,7 +94,7 @@ function parseresults(g::Graph, raw_results)
 end
 
 
-function parsequery(g::Graph, raw_query_result)
+function parsequery(g::Graph, raw_query_result::Array{Any})
     stats = parsestatistics(raw_query_result[length(raw_query_result)])
     header, results = parseresults(g, raw_query_result[1:length(raw_query_result)-1])
     stats, header, results
@@ -113,7 +102,7 @@ end
 
 
 struct ResultHeaderEntry
-    r_entry_type::UInt
+    r_entry_type::Int
     r_entry_name::String
     ResultHeaderEntry(raw_header_entry) = new(raw_header_entry[1], raw_header_entry[2])
 end
@@ -123,7 +112,7 @@ struct QueryResult
     statistics::Dict{String, Float64}
     header::Union{Vector{ResultHeaderEntry}, Nothing}
     results::Union{Vector{Any}, Nothing}
-    function QueryResult(g::Graph, raw_query_result)
+    function QueryResult(g::Graph, raw_query_result::Array{Any})
         stats, header, results = parsequery(g, raw_query_result)
         new(stats, header, results)
     end
