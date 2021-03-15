7a243c4a442fb800b51b3f9cdd35687cf8bc58b7
ikudrautsau <ihar.kudrautsau@gmail.com>
2021-03-16 00:45:39 +0300

    rework runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,31 +5,19 @@ using RedisGraph: Graph, Node, Edge, Path, addnode!, addedge!, commit, delete, q
 
 function creategraph()
     db_conn = RedisConnection()
-    g = Graph("test1", db_conn)
+    g = Graph("TestGraph", db_conn)
     return g
 end
 
 
 function simplerelation!(g::Graph)
-    node1 = Node("test1")
-    node2 = Node("test2")
-    edge = Edge("edge1", node1, node2)
+    node1 = Node("FirstSimpleNode", Dict("IntProp" => 1, "StringProp" => "node prop", "BoolProp" => true))
+    node2 = Node("SecondSimpleNode")
+    edge = Edge("SimpleEdge", node1, node2, Dict("IntProp" => 1, "StringProp" => "node prop", "BoolProp" => false))
 
     addnode!(g, node1)
     addnode!(g, node2)
     addedge!(g, edge)
-    res = commit(g)
-end
-
-
-function rel_withprops!(g::Graph)
-    node3 = Node("test3", Dict("a" => 1))
-    node4 = Node("test4")
-    edge2 = Edge(1, "edge2", node3, node4, Dict("b" => 1))
-
-    addnode!(g, node3)
-    addnode!(g, node4)
-    addedge!(g, edge2)
     res = commit(g)
 end
 
@@ -39,20 +27,44 @@ function deletegraph!(g::Graph)
 end
 
 
-g = creategraph()
-try
-    simplerelation!(g)
+@testset "RedisGraph.jl tests" begin
+    @testset "check simple types" begin
+        g = creategraph()
+        @test query(g, "RETURN null").results[1] === nothing
+        @test query(g, "RETURN 2").results[1] == 2
+        @test query(g, "RETURN 2.0").results[1] == 2.0
+        @test query(g, "RETURN true").results[1] == true
+        @test query(g, "RETURN [1, 2, 'test', 3.0, false]").results[1] == [1, 2, "test", 3.0, false]
+    end
+    @testset "check simple relation" begin
+        g = creategraph()
+        try
+            simplerelation!(g)
 
-    @test query(g, "RETURN null").results[1] === nothing
-    @test query(g, "RETURN 2").results[1] == 2
-    @test query(g, "RETURN 2.0").results[1] == 2.0
-    @test query(g, "RETURN true").results[1] == true
-    @test query(g, "RETURN [1, 2, 'test', 3.0, false]").results[1] == [1, 2, "test", 3.0, false]
-    @test typeof(query(g, "MATCH (n1)-[e]->(n2) RETURN n1").results[1]) == Node
-    @test typeof(query(g, "MATCH (n1)-[e]->(n2) RETURN e").results[1]) == Edge
-    @test typeof(query(g, "MATCH p=(n1)-[e]->(n2) RETURN p").results[1]) == Path
-
-    rel_withprops!(g)
-finally
-    deletegraph!(g)
+            q = query(g, "MATCH (n1)-[e]->(n2) RETURN n1, e, n2").results
+            node1, edge, node2 = q[1:3]
+            
+            @test typeof(node1) == Node
+            @test node1.label == "FirstSimpleNode"
+            @test node1.properties == Dict("IntProp" => 1, "StringProp" => "node prop", "BoolProp" => true)
+            @test typeof(edge) == Edge
+            @test edge.relation == "SimpleEdge"
+            @test edge.properties == Dict("IntProp" => 1, "StringProp" => "node prop", "BoolProp" => false)
+            @test edge.src_node == node1.id
+            @test edge.dest_node == node2.id
+            @test typeof(node2) == Node
+            @test node2.label == "SecondSimpleNode"
+        finally
+            deletegraph!(g)
+        end
+    end
+    @testset "check path type" begin
+        g = creategraph()
+        try
+            simplerelation!(g)
+            @test typeof(query(g, "MATCH p=(n1)-[e]->(n2) RETURN p").results[1]) == Path
+        finally
+            deletegraph!(g)
+        end
+    end
 end
